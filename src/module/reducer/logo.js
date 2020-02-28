7733994b71331bf135a6763a876340258bed4f4b
LEE JUCHAN <juchanei@naver.com>
2020-02-29 02:59:38 +0900

    refactor logo reducer

--- src/module/reducer/logo.js
+++ src/module/reducer/logo.js
@@ -1,6 +1,10 @@
 import { logo as types } from '../actionTypes'
 
-export default (state = {}, action) => {
+const initialState = {
+    speed: 1,
+}
+
+export default (state = initialState, action) => {
     switch (action.type) {
         case types.SPEED_INCREMENT: return {
             ...state,
@@ -10,9 +14,6 @@ export default (state = {}, action) => {
             ...state,
             speed: 2 < state.speed ? state.speed - 1 : 1,
         }
-        default: return {
-            ...state,
-            speed: 1,
-        }
+        default: return state
     }
 }
